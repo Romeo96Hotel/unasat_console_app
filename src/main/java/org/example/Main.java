8f4198bd653d6abb1fb7ba8c337e51f600d3934b
romeo hotel <90534544+Romeo96Hotel@users.noreply.github.com>
2023-05-19 20:52:59 -0300

    Remove stupidity

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        /*UserInterface ui = new UserInterface();
-        ui.start();*/
-
-        ContestantDAO dao = new ContestantDAO();
-
-        dao.getAllContestants();
+        UserInterface ui = new UserInterface();
+        ui.start();
     }
 }
